38d34b3a6ae64657210043a0cbcff7efca481707
sohaibs-liberary <muhammadsohaibshakeel18@gmail.com>
2026-05-14 15:24:30 +0500

    make data in chnks

--- Goroutinues.go
+++ Goroutinues.go
@@ -1,30 +1,25 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
-	"runtime"
-	"strings"
+	"sync"
 	"time"
-	 "sync"
 )
 
-type result struct{
-
-	word int
-	letter int
-	lineno int 
-	space int
-	valve int
-    paragraphes int
-    specialchar int
-
+type result struct {
+	words      int
+	letters    int
+	lines      int
+	spaces     int
+	vowels     int
+	paragraphs int
+	special    int
 }
 
-// chunk  data 
+// chunk  data
 
-func countChunk(chunk []rune) result  {
+func countChunk(chunk []rune) result {
 
 	var r result
 
@@ -33,47 +28,47 @@ func countChunk(chunk []rune) result  {
 	for _, ch := range chunk {
 
 		switch ch {
-         case '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=','{', '}', '[', ']', '|', '\\', ':', ';', '"', '\'', '<', '>', ',', '.', '?', '/', '~', '`':
+		case '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', '\\', ':', ';', '"', '\'', '<', '>', ',', '.', '?', '/', '~', '`':
 			r.special++
 
-		case'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
-			 r.vowels++
-			}
+		case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+			r.vowels++
+		}
 		switch ch {
-			case ' ':
-				r.spaces++
-				inWord = false
-			case '\t':
-				inWord = false
-			case '\n':
-		    	r.lines++
-				inWord = false
-			default:
-				if !inWord {
-					r.words++
-					inWord = true
-  } 
-}
+		case ' ':
+			r.spaces++
+			inWord = false
+		case '\t':
+			inWord = false
+		case '\n':
+			r.lines++
+			inWord = false
+		default:
+			if !inWord {
+				r.words++
+				inWord = true
+			}
+		}
 
+	}
+	return r
 }
-}
-
 
 func main() {
 	start := time.Now()
 
 	fileData, _ := os.ReadFile("word.txt")
 
-	rune := []rune(string(fileData))
-	total := len(rune)
+	runes := []rune(string(fileData))
+	total := len(runes)
 
 	// Number of Goroutinues
 
 	numWorkers := 2
-	chunkSize := (total + numWorkers  -1 )/numWorkers
-	
-	resultCh :=  make(chan result, numWorkers)
-	 
+	chunkSize := (total + numWorkers - 1) / numWorkers
+
+	resultCh := make(chan result, numWorkers)
+
 	var wg sync.WaitGroup
 
 	for i := 0; i < numWorkers; i++ {
@@ -84,62 +79,45 @@ func main() {
 		if end > total {
 
 			end = total
-			
+
 		}
 		if start >= total {
 
 			break
-			
+
 		}
-		 wg.Add(1)
-		 go func (chunk [] rune)  {
-			
-		 }
+		wg.Add(1)
+		go func(chunk []rune) {
+			defer wg.Done()
+			resultCh <- countChunk(chunk)
+		}(runes[start:end])
 	}
 
+	go func() {
+		wg.Wait()
+		close(resultCh)
+	}()
 
-	
+	var final result
+	for r := range resultCh {
+		final.words += r.words
+		final.letters += r.letters
+		final.lines += r.lines
+		final.special += r.special
+		final.spaces += r.spaces
+		final.paragraphs += r.paragraphs
+		final.vowels += r.vowels
+	}
+
+	fmt.Printf("Total Words: %d\n", final.words)
+	fmt.Printf("Total Letters: %d\n", final.letters)
+	fmt.Printf("Total Lines: %d\n", final.lines)
+	fmt.Printf("Total Special: %d\n", final.special)
+	fmt.Printf("Total Spaces: %d\n", final.spaces)
+	fmt.Printf("Total Paragraphs: %d\n", final.paragraphs)
+	fmt.Printf("Total Vowels:%d\n", final.vowels)
 	fmt.Println("Execution time:", time.Since(start))
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -173,7 +151,7 @@ func main() {
 // 		words++
 // 		inWord = true
 // 	}
-	
+
 // }
 
 // 	fmt.Printf("Total Words %d \n", words)
